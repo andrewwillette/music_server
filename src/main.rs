7f9ea664cd24295bca29dc24379ebd10804702ec
andrewwillette <willette.andrew1846@gmail.com>
2023-01-26 22:14:23 -0600

    master: defining consts, defined get_soundcloud_urls

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, App, HttpServer, Responder};
 
-#[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
+static HOST: &str = "127.0.0.1";
+const PORT: u16 = 8080;
+
+#[get("/get-soundcloud-urls")]
+async fn greet() -> impl Responder {
+    let urls = get_soundcloud_urls();
+    format!("{:?}", urls)
 }
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(greet))
-        .bind(("127.0.0.1", 8080))?
+        .bind((HOST, PORT))?
         .run()
         .await
+}
+
+fn get_soundcloud_urls() -> Vec<String> {
+    let soundcloud_urls = vec![String::from("hi"), String::from("goodbye")];
+    return soundcloud_urls;
 }

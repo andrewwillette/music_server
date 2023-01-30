cb18a17557916292e570c2804481c4708e57a347
andrewwillette <willette.andrew1846@gmail.com>
2023-01-29 18:34:07 -0600

    master: can now access data in post body

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,38 @@
-use actix_web::{get, App, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
 
 static HOST: &str = "127.0.0.1";
 const PORT: u16 = 8080;
 
-#[get("/get-soundcloud-urls")]
-async fn greet() -> impl Responder {
-    let urls = get_soundcloud_urls();
+#[derive(Serialize, Deserialize)]
+struct SoundcloudUrl {
+    url: String,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(get_soundcloud_urls)
+            .service(add_soundcloud_url)
+    })
+    .bind((HOST, PORT))?
+    .run()
+    .await
+}
+
+#[get("/soundcloud-urls")]
+async fn get_soundcloud_urls() -> impl Responder {
+    let urls = get_db_soundcloud_urls();
     format!("{:?}", urls)
 }
 
-#[actix_web::main] // or #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(greet))
-        .bind((HOST, PORT))?
-        .run()
-        .await
+#[post("/soundcloud-url")]
+async fn add_soundcloud_url(_json: web::Json<SoundcloudUrl>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
-fn get_soundcloud_urls() -> Vec<String> {
+fn get_db_soundcloud_urls() -> Vec<String> {
     let soundcloud_urls = vec![String::from("hi"), String::from("goodbye")];
     return soundcloud_urls;
 }

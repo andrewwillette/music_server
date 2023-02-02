de7ab8a4145d8781a0b406210b8c6d855fee0c51
andrewwillette <willette.andrew1846@gmail.com>
2023-02-01 22:03:57 -0600

    master: db insert and get now working, building in init_db for idempotent runs

--- src/db.rs
+++ src/db.rs
@@ -1,13 +1,38 @@
 use rusqlite::{Connection, Result};
 
+const DB_PATH: &str = "./music_server.db";
+
 #[derive(Debug)]
 pub struct SoundcloudUrl {
     id: i32,
     url: String,
 }
 
+pub fn init_db() -> Result<()> {
+    const SOUNDCLOUD_URL_TABLE_EXISTS_QUERY: &str =
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='soundcloudurl';";
+    const CREATE_SOUNDCLOUD_URL_TABLE_QUERY: &str = "CREATE TABLE soundcloudurl (
+        id    INTEGER PRIMARY KEY,
+        url   TEXT NOT NULL,
+        data  BLOB
+    )";
+    let conn = Connection::open(DB_PATH)?;
+    let mut stmt = conn.prepare(SOUNDCLOUD_URL_TABLE_EXISTS_QUERY)?;
+    let exists_rows = stmt.query_map((), |row| row.get(0))?;
+    conn.execute(SOUNDCLOUD_URL_TABLE_EXISTS_QUERY, ())?;
+    // conn.execute(
+    //     "CREATE TABLE soundcloudurl (
+    //         id    INTEGER PRIMARY KEY,
+    //         url   TEXT NOT NULL,
+    //         data  BLOB
+    //     )",
+    //     (), // empty list of parameters.
+    // )?;
+    return Ok(());
+}
+
 pub fn insert_soundcloud_url(url: &String) -> Result<()> {
-    let conn = Connection::open_in_memory()?;
+    let conn = Connection::open(DB_PATH)?;
     conn.execute(
         "CREATE TABLE soundcloudurl (
             id    INTEGER PRIMARY KEY,
@@ -27,13 +52,43 @@ pub fn insert_soundcloud_url(url: &String) -> Result<()> {
     return Ok(());
 }
 
+pub fn get_soundcloud_urls() -> Result<Vec<SoundcloudUrl>> {
+    let conn = Connection::open(DB_PATH)?;
+    let mut stmt = conn.prepare("SELECT id, url FROM soundcloudurl")?;
+    let soundcloud_url_iter = stmt.query_map([], |row| {
+        Ok(SoundcloudUrl {
+            id: row.get(0)?,
+            url: row.get(1)?,
+        })
+    })?;
+    let mut soundcloud_urls = Vec::new();
+    for soundcloud_url in soundcloud_url_iter {
+        soundcloud_urls.push(soundcloud_url?);
+    }
+    Ok(soundcloud_urls)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
+    fn test_init_db() {
+        let url = String::from("https://soundcloud.com/thisismyurl");
+        assert_eq!(insert_soundcloud_url(&url), Ok(()));
+    }
+
+    #[test]
     fn test_insert_soundcloud_url() {
         let url = String::from("https://soundcloud.com/thisismyurl");
         assert_eq!(insert_soundcloud_url(&url), Ok(()));
+    }
+
+    #[test]
+    fn test_get_soundcloud_url() {
+        let url = String::from("https://soundcloud.com/thisismyurl");
+        assert_eq!(insert_soundcloud_url(&url), Ok(()));
+        let soundcloud_urls = get_soundcloud_urls().unwrap();
+        print!("{:?}\n", soundcloud_urls);
     }
 }
